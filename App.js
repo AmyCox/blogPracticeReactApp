beedf84d0c8591bc782bbf4a89fdc1f0753a77c3
Amy Cox <amy_cox@me.com>
2021-08-12 11:51:07 +0100

    refactor of createDataContext

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
 const Stack = createStackNavigator();
 
-
-const App= () => {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Index">
-        <Stack.Screen name="Index" component={IndexScreen} options={{ title: "Blogs" }}/>
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: "Blogs" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default () => {
-  
-  return <BlogProvider>
-    <App/>
-  </BlogProvider>
-
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
 };
